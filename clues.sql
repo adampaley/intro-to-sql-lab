24f2e41c0d64a72124219ae944bd8e2746796c2a
Adam Paley <apaley95@gmail.com>
2025-02-22 18:26:43 -0500

    Solved Clue #1

--- clues.sql
+++ clues.sql
@@ -2,6 +2,13 @@
  
 -- Write SQL query here
 
+SELECT name, population 
+FROM countries 
+WHERE region = 'Southern Europe'
+ORDER BY population ASC 
+LIMIT 1;
+
+-- Holy See (Vatican City State), population: 1000
 
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in this country's officially recognized language. Check our databases and find out what language is spoken in this country, so we can call in a translator to work with you.
 
