3360e61e8853087a369157c3854ff27a07295fbc
Adam Paley <apaley95@gmail.com>
2025-02-22 19:19:29 -0500

    Solved Clue #7

--- clues.sql
+++ clues.sql
@@ -81,3 +81,9 @@ WHERE countries.name = 'Brazil';
 
 
 -- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
+
+SELECT name
+FROM cities
+WHERE population = '91084';
+
+-- Santa Monica
