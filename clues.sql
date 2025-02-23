66197f0bd3dff4c30c5a4acf800c7de4634fa987
Adam Paley <apaley95@gmail.com>
2025-02-22 19:10:05 -0500

    Solved Clue #5

--- clues.sql
+++ clues.sql
@@ -48,7 +48,13 @@ AND NOT countries.name = cities.name;
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
 
 -- Write SQL query here
+SELECT countries.name AS country, cities.name AS city
+FROM countries
+JOIN cities on countries.code = cities.countrycode
+WHERE cities.name LIKE 'Serr%'
+AND countries.continent = 'South America';
 
+-- Brazil
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
 -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
